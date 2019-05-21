71f0003aa7c390f7495c3c0b3581d3450ffccb36
Lajos Neto <lajosnetogit@gmail.com>
2019-05-20 22:45:39 -0400

    Tensorflow - new Tensorflow mimic implementation
    Add new Python implementation that mimics Tensorflow behaviour.
    Delete Neural network implementation form scratch in Python.

---
+++ tensorflow/tensorflow_mimic.py
@@ -0,0 +1,12 @@
+#%%
+class Graph():
+
+    def __init__(self):
+        self.operations = []
+        self.placeholders = []
+        self.variables = []
+        self.constants = []
+    
+    def set_as_default(self):
+        global _default_graph
+        _default_graph = self
