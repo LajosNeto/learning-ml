5abe3aaeddf53c0222a5c0c40f08dc3d9c791d6a
Lajos Neto <lajosnetogit@gmail.com>
2019-05-21 21:22:05 -0400

    Tensorflow mimic - Add new operations
    Add Base operation class.
    Add Binary Operation class.
    Add Addition operation class.
    Add Multiply operation class.
    Add Divide operation class.
    Add Matmul operation class.

--- tensorflow/tensorflow_mimic.py
+++ tensorflow/tensorflow_mimic.py
@@ -1,6 +1,6 @@
+import numpy as np
 #%%
 class Graph():
-
     def __init__(self):
         self.operations = []
         self.placeholders = []
@@ -10,3 +10,42 @@ class Graph():
     def set_as_default(self):
         global _default_graph
         _default_graph = self
+
+
+class Operation():
+    def __init__(self, input_nodes=None):
+        self.input_nodes = input_nodes
+        self.output = None
+
+        _default_graph.operations.append(self)
+    
+    def forward(self):
+        pass
+    
+    def backward(self):
+        pass
+
+
+class BinaryOperation(Operation):
+    def __init__(self, a, b):
+        super().__init__([a,b])
+
+
+class Add(BinaryOperation):
+    def forward(self, a, b):
+        return a + b
+
+
+class Multiply(BinaryOperation):
+    def forward(self, a, b):
+        return a * b
+
+
+class Divide(BinaryOperation):
+    def forward(self, a, b):
+        return np.true_divide(a, b)
+
+
+class Matmul(BinaryOperation):
+    def forward(self, a, b):
+        return a.dot(b)
